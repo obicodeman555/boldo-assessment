342b18cedee190723f0e56c52f5b6834ac9e9102
Obinna Uko <obicodeman@yahoo.com>
2024-01-15 15:04:32 +0100

    responsive header for web and mobile screen navigations

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./header.scss";
 import primaryLogo from "assets/svgs/primary-logo.svg";
+import { Icon } from '@iconify/react';
+import { useState } from "react";
 
 const Header = () => {
+  const [isHiddenMobileNav, setIsHiddenMobileNav] = useState(false)
   return (
     <header className="header-home">
       <div className="container">
@@ -33,6 +36,38 @@ const Header = () => {
               </Link>
             </li>
           </ul>
+
+          <div className="mobile-nav-block">
+            <button type="button" className={isHiddenMobileNav ? "mobile-nav__toggler active" : "mobile-nav__toggler"} onClick={() => setIsHiddenMobileNav(prev => !prev)}>
+              <Icon icon="mdi:hamburger-open" />
+            </button>
+
+            <nav className={isHiddenMobileNav ? "mobile-nav show" : "mobile-nav hidden"}>
+              <ul className="mobile-nav__list">
+                <li>
+                  <Link to="" className="mobile-nav__link">
+                    Product
+                  </Link>
+                </li>
+                <li>
+                  <Link to="" className="mobile-nav__link">
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link to="" className="mobile-nav__link">
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to="" className="button button-link button--primary">
+                    log in
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+
+          </div>
         </nav>
       </div>
     </header>
